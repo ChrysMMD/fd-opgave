ab22ac599cc0d14b022e418128eabbdc7c07d1d4
Chrys <chjo0001@stud.kea.dk>
2025-03-15 23:15:51 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-<<<<<<< HEAD
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -12,14 +11,4 @@ export default defineConfig({
     svg: true,
     clientPrerender: true,
   },
-=======
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
->>>>>>> 17a2bcf (Initial commit from Astro)
 });
